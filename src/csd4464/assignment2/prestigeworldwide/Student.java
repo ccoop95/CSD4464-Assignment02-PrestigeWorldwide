4d9abdfc2aab3c24e0e39d20770fa62f41578542
Chance Cooper <c0659824@mylambton.ca>
2017-01-18 11:58:38 -0500

    Student Class Built

--- src/csd4464/assignment2/prestigeworldwide/Student.java
+++ src/csd4464/assignment2/prestigeworldwide/Student.java
@@ -5,10 +5,100 @@
  */
 package csd4464.assignment2.prestigeworldwide;
 
+import java.util.Objects;
+
 /**
  *
  * @author c0659824
  */
 public class Student {
+    String name;
+    String id;
+    String gender;
+    double grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public Student(String name, String id, String gender, double grade) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
+    }
+    public Student() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", id=" + id + ", gender=" + gender + ", grade=" + grade + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.gender);
+        hash = 59 * hash + (int) (Double.doubleToLongBits(this.grade) ^ (Double.doubleToLongBits(this.grade) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (Double.doubleToLongBits(this.grade) != Double.doubleToLongBits(other.grade)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        return true;
+    }
     
 }
