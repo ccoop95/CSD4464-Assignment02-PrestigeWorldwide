d50aa628084398f157d2f8297c0b3d3efa602f29
Chance Cooper <c0659824@mylambton.ca>
2017-01-18 11:48:24 -0500

    Initial Commit

---
+++ src/csd4464/assignment2/prestigeworldwide/CSD4464Assignment2PrestigeWorldwide.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.assignment2.prestigeworldwide;
+
+/**
+ *
+ * @author c0659824
+ */
+public class CSD4464Assignment2PrestigeWorldwide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
