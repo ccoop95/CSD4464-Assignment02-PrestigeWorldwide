f21cb9c006838b387e8a1cd65822d8f1ac213649
Travis Kelly <c0668586@mylambton.ca>
2017-01-25 11:41:55 -0500

    Added to the Course Class nothing good

--- src/csd4464/assignment2/prestigeworldwide/Course.java
+++ src/csd4464/assignment2/prestigeworldwide/Course.java
@@ -6,7 +6,9 @@
 package csd4464.assignment2.prestigeworldwide;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -16,7 +18,9 @@ public class Course extends Student{
     List<Student> students = new ArrayList<>();
     public List<Student> Course;
     
-    
+    public void Course(){
+        getAll();
+    }
     
     public void Course(List<Student> Course) {
             this.Course = Course;
@@ -34,15 +38,28 @@ public class Course extends Student{
         students.remove(stu);
     }
     
+    public void remove(String id){
+        students.remove(id);
+    }
+    
+    public void remove(int position){
+        students.remove(position);
+    }
+       
+    public void get(String id){
+       // students.indexOf();
+    }
     
     
     
+    public void getAllByGender(String gender){
+        //HashSet<Student> students = new HashSet<>();
+        
+    }
     
-    
-    
-    
-    
-    
-    
-    
+    @Override
+    public String toString() {
+        for (string )
+        return "Student{" + "name=" + name + ", id=" + id + ", gender=" + gender + ", grade=" + grade + '}';
+    }
 }
